77ca3373d06d233ba698d49efd5adcb0eec4128f
cigetbudi <cigetbudi@gmail.com>
2025-06-08 23:21:07 +0700

    Adding needed Nugget Packages

--- src/NetBoiler.Infrastructure/obj/Debug/net10.0/NetBoiler.Infrastructure.AssemblyInfo.cs
+++ src/NetBoiler.Infrastructure/obj/Debug/net10.0/NetBoiler.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetBoiler.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dfa703db75e50efb9f478a70627cf6ed3676ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetBoiler.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetBoiler.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
